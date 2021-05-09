f1a29cb74c578dac0648585ac55c9d059a579687
Владислав Трубников <vladtrubn@gmail.com>
2021-05-10 05:55:05 +0700

    Более удобные для клиента методы комментов

--- Documents/source/repos/TechMeCookServer/TechMeCookServer/Controllers/RecipesController.cs
+++ Documents/source/repos/TechMeCookServer/TechMeCookServer/Controllers/RecipesController.cs
@@ -36,6 +36,7 @@ namespace TechMeCookServer.Controllers
 
         private String detailUriTemplate = "recipes/{0}/information?apiKey={1}";
         private String randomCollectionUriTemplate = "recipes/random?number={0}&tags={1}&apiKey={2}";
+        private String filteredCollectionUriTemplate = "recipes/complexSearch?query={0}&includeIngredients={1}&equipment={2}&diet={3}&intolerances={4}";
 
         public RecipesController(UserManager<ApplicationUser> userManager, ILogger<RecipesController> logger, IHttpClientService httpClientService, ApplicationDbContext context)
         {
@@ -45,24 +46,6 @@ namespace TechMeCookServer.Controllers
             this.context = context;
             this.userManager = userManager;
         }
-
-        /* [HttpGet("{recipeId}/information")]
-         public async Task<IActionResult> GetDetail(long recipeId, String apiKey)
-         {
-             String actualUri = string.Format(detailUriTemplate, recipeId, httpClientService.GetApiKey());
-             var response = await httpClient.GetAsync(actualUri);
-             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-             response.EnsureSuccessStatusCode();
-             return Ok(await response.Content.ReadAsStringAsync());
-
-            var basereturn =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
-             {
-                 Date = DateTime.Now.AddDays(index),
-                 TemperatureC = rng.Next(-20, 55),
-                 Summary = Summaries[rng.Next(Summaries.Length)]
-             }).ToArray();
-             return basereturn;
-         } */
 
         [HttpGet("{recipeId}/information")]
         public async Task<IActionResult> GetDetail(long recipeId, String apiKey)
@@ -96,14 +79,6 @@ namespace TechMeCookServer.Controllers
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             response.EnsureSuccessStatusCode();
             return Ok(jsonResponse);
-
-            /*  var basereturn =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
-              {
-                  Date = DateTime.Now.AddDays(index),
-                  TemperatureC = rng.Next(-20, 55),
-                  Summary = Summaries[rng.Next(Summaries.Length)]
-              }).ToArray();
-              return basereturn;*/
         }
 
 
@@ -111,6 +86,16 @@ namespace TechMeCookServer.Controllers
         public async Task<IActionResult> GetRandomCollection(int? number, String? tags, String apiKey)
         {
             String actualUri = string.Format(randomCollectionUriTemplate, number, tags, httpClientService.GetApiKey());
+            var response = await httpClient.GetAsync(actualUri);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            response.EnsureSuccessStatusCode();
+            return Ok(await response.Content.ReadAsStringAsync());
+        }
+
+        [HttpGet("complex")]
+        public async Task<IActionResult> GetFilteredCollection(String? query, String? includeIngredients, String? equipment, String? diet, String? intolerances, String apiKey)
+        {
+            String actualUri = string.Format(randomCollectionUriTemplate, query, includeIngredients, equipment,  diet, intolerances, httpClientService.GetApiKey());
             var response = await httpClient.GetAsync(actualUri);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             response.EnsureSuccessStatusCode();
